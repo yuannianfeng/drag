5af88b9829712b8561fb452222f0bff3d5d92a21
yuannianfeng <847078795@qq.com>
2017-04-12 16:07:48 +0800

    changes drag.js

--- drag.js
+++ drag.js
@@ -2,5 +2,8 @@
  * Created by 15066 on 2017/4/12.
  */
 function drag() {
-    var id=document.getElementById('id')
+    var id=document.getElementById('id');
+    id.onclick=function () {
+        alert('hello')
+    }
 }
